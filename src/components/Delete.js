64d8ac4b7b98f432ef940599b2130d175995a964
+ <+>
2022-09-10 07:51:57 +0300

    updting readme.md

--- src/components/Delete.js
+++ src/components/Delete.js
@@ -15,10 +15,9 @@ export const Delete = ({onDelete, car}) => {
              <h5 className="text-gray-900 text-xl leading-tight font-medium mb-2">{car.name}</h5>
              <p className="text-gray-700 text-base mb-4 " >{car.country}</p>
              <p className="text-gray-700 text-base mb-4 " >{car.price}</p>
-           
-                     <button type="button" className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"onClick={handleDeleteClick} >Delete</button>
-                     </div>
-                     </div>
+           <button type="button" className=" inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"onClick={handleDeleteClick}>Delete</button>
+          </div>
+          </div>
         
       )
 
