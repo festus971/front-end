0ba1f40b1c59fc3f9b7cb9ee9f90c5a91b37cd49
+ <+>
2022-09-11 14:56:58 +0300

    updating home

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,7 +4,6 @@ function Home() {
     return (
         <div>
              <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-        {/* <img class="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="/sarah-dayan.jpg" alt="" width="384" height="512"> */}
         <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
           <blockquote>
             <p class="text-lg font-medium">
@@ -20,7 +19,8 @@ function Home() {
             </div>
           </figcaption>
         </div>
-      </figure></div>
+      </figure>
+      </div>
     )
     
 }
